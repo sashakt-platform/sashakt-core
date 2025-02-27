165b2c448b4aa2ce51bd255895c5c15f37bda643
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-02-27 12:10:17 +0530

    Added Route for Organization API in Main file

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from app.routers.location import location
+from app.routers import organization
 
 app = FastAPI()
+
+app.include_router(location.router)
+app.include_router(organization.router)
 
 
 @app.get("/")
