734d0fc8b3690516260ed8b7d8c4998ee80284f7
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-02-27 11:31:26 +0530

    Moved Organization Model to seperate file

--- src/app/models/location.py
+++ src/app/models/location.py
@@ -15,12 +15,6 @@ class BaseModel(SQLModel):
     is_active: Optional[bool] = Field(nullable=True)
 
 
-class Organization(BaseModel, table=True):
-    name: str = Field(nullable=False)
-    description: Optional[str] = Field(default=None, nullable=True)
-    is_deleted: bool = Field(default=False, nullable=False)
-
-
 class Country(BaseModel, table=True):
     name: str = Field(nullable=False)
     states: Optional[List["State"]] = Relationship(back_populates="country")
---
+++ src/app/models/organization.py
@@ -0,0 +1,9 @@
+from location import BaseModel
+from typing import Optional
+from sqlmodel import Field
+
+
+class Organization(BaseModel, table=True):
+    name: str = Field(nullable=False)
+    description: Optional[str] = Field(default=None, nullable=True)
+    is_deleted: bool = Field(default=False, nullable=False)
