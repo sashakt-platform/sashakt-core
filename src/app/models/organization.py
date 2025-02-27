414818b61168f5e3846e92549c97760031635896
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-02-27 15:31:43 +0530

    Formatted with ruff

--- src/app/models/organization.py
+++ src/app/models/organization.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from sqlmodel import Field,SQLModel
+from sqlmodel import Field, SQLModel
 from datetime import datetime, timezone
+
 
 class Organization(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
