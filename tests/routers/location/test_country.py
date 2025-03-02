beb477f9eeeda5f912a1c8d8306d02f39aff508e
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-02 17:07:06 +0530

    Test Cases for Get,Put,Update,get_by_id APIs for country

--- tests/routers/location/test_country.py
+++ tests/routers/location/test_country.py
@@ -1,4 +1,7 @@
 from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from app.models.location import Country
 
 
 def test_create_country(client: TestClient):
@@ -6,3 +9,40 @@ def test_create_country(client: TestClient):
     data = response.json()
     assert response.status_code == 200
     assert data["name"] == "India"
+
+
+def test_get_country(client: TestClient, session: Session):
+    dubai = Country(name="Dubai")
+    austria = Country(name="Austria")
+    session.add(dubai)
+    session.add(austria)
+    session.commit()
+    response = client.get("/location/country/")
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 2
+    assert data[0]["name"] == "Dubai"
+    assert data[1]["name"] == "Austria"
+
+
+def test_get_country_by_id(client: TestClient, session: Session):
+    srilanka = Country(name="Srilanka")
+    session.add(srilanka)
+    session.commit()
+    response = client.get("/location/country/1")
+    data = response.json()
+    assert response.status_code == 200
+    assert data["name"] == "Srilanka"
+    assert data["id"] == 1
+
+
+def test_update_country(client: TestClient, session: Session):
+    australia = Country(name="Australiaaa")
+    session.add(australia)
+    session.commit()
+    response = client.put("/location/country/1", json={"name": "Australia"})
+    data = response.json()
+    assert response.status_code == 200
+    assert data["name"] == "Australia"
+    assert data["id"] == 1
+    assert data["name"] != "Australiaaa"
