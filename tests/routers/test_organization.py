0e4986a1a57bc97a6b8ce6f73d51998abd148fba
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-08 23:49:00 +0530

    Updated Test Cases for Organization

--- tests/routers/test_organization.py
+++ tests/routers/test_organization.py
@@ -1,5 +1,3 @@
-import re
-
 from fastapi.testclient import TestClient
 from sqlmodel import Session
 
@@ -7,11 +5,17 @@ from app.models.organization import Organization
 
 
 def test_create_organization(client: TestClient):
-    response = client.post("/organization/", json={"name": "Jay Sangh"})
+    name = "Jay Sangh"
+    description = "It is a non profit organization"
+    response = client.post(
+        "/organization/", json={"name": name, "description": description}
+    )
     data = response.json()
     assert response.status_code == 200
-    assert data["name"] == "Jay Sangh"
+    assert data["name"] == name
+    assert data["description"] == description
     assert data["id"] == 1
+    assert data["is_active"] is None
 
 
 def test_read_organization(client: TestClient, session: Session):
@@ -20,46 +24,109 @@ def test_read_organization(client: TestClient, session: Session):
     assert response.status_code == 200
     assert len(data) == 0
     jal_vikas = Organization(name="Jal Vikas")
+    maha_vikas = Organization(name="Maha Vikas", description="Lets Work for Humanity")
     session.add(jal_vikas)
+    session.add(maha_vikas)
     session.commit()
     response = client.get("/organization/")
     data = response.json()
     assert response.status_code == 200
-    assert len(data) == 1
-    assert data[0]["name"] == "Jal Vikas"
-    assert data[0]["id"] == 1
+    assert len(data) == 2
+    assert data[0]["name"] == jal_vikas.name
+    assert data[0]["description"] is None
+    assert data[0]["id"] == jal_vikas.id
+    assert data[0]["is_active"] is None
+
+    assert data[1]["name"] == maha_vikas.name
+    assert data[1]["description"] == maha_vikas.description
+    assert data[1]["id"] == maha_vikas.id
 
 
 def test_read_organization_by_id(client: TestClient, session: Session):
     response = client.get("/organization/1")
     assert response.status_code == 404
     assert response.json() == {"detail": "Organization not found"}
+
     jal_vikas = Organization(name="Jal Vikas")
+    maha_vikas = Organization(name="Maha Vikas", description="Lets Work for Humanity")
     session.add(jal_vikas)
+    session.add(maha_vikas)
     session.commit()
-    response = client.get("/organization/1")
+
+    response = client.get(f"/organization/{jal_vikas.id}")
     data = response.json()
     assert response.status_code == 200
-    assert data["name"] == "Jal Vikas"
-    assert data["id"] == 1
+    assert data["name"] == jal_vikas.name
+    assert data["id"] == jal_vikas.id
+    assert data["description"] is None
+
+    response = client.get(f"/organization/{maha_vikas.id}")
+    data = response.json()
+    assert response.status_code == 200
+    assert data["name"] == maha_vikas.name
+    assert data["id"] == maha_vikas.id
+    assert data["description"] == maha_vikas.description
+    assert data["is_active"] is None
 
 
 def test_update_organization(client: TestClient, session: Session):
-    response = client.put("/organization/1", json={"name": "Jal Vikas"})
+    response = client.put(
+        "/organization/1", json={"name": "Jal Vikas", "description": None}
+    )
     assert response.status_code == 404
     assert response.json() == {"detail": "Organization not found"}
+
     jal_vikas = Organization(name="Jal Vikas")
     session.add(jal_vikas)
     session.commit()
-    response = client.put("/organization/1", json={"name": "Jal Vikas Sangh"})
+    response = client.put(
+        f"/organization/{jal_vikas.id}",
+        json={"name": "Jal Vikas Sangh", "description": None},
+    )
     data = response.json()
     assert response.status_code == 200
     assert data["name"] == "Jal Vikas Sangh"
-    assert data["id"] == 1
+    assert data["id"] == jal_vikas.id
     assert data["name"] != "Jal Vikas"
-    response = client.put("/organization/2", json={"name": "Jal Vikas Sangh"})
+    assert data["description"] is None
+
+    response = client.put(
+        f"/organization/{jal_vikas.id}",
+        json={"name": jal_vikas.name, "description": "Humanity First!!"},
+    )
+    data = response.json()
+    assert response.status_code == 200
+    assert data["name"] == jal_vikas.name
+    assert data["id"] == jal_vikas.id
+    assert data["description"] == "Humanity First!!"
+
+    response = client.put(
+        "/organization/2",
+        json={"name": "Jal Vikas Sangh", "description": "Serve the Poor"},
+    )
     assert response.status_code == 404
     assert response.json() == {"detail": "Organization not found"}
+
+
+def test_visibility_organization(client: TestClient, session: Session):
+    jal_vikas = Organization(name="Jal Vikas")
+    session.add(jal_vikas)
+    session.commit()
+    response = client.patch(f"/organization/{jal_vikas.id}", params={"is_active": True})
+    data = response.json()
+    assert response.status_code == 200
+    assert data["name"] == jal_vikas.name
+    assert data["is_active"] is True
+    assert data["is_active"] is not False and not None
+
+    response = client.patch(
+        f"/organization/{jal_vikas.id}", params={"is_active": False}
+    )
+    data = response.json()
+    assert response.status_code == 200
+    assert data["id"] == jal_vikas.id
+    assert data["is_active"] is False
+    assert data["is_active"] is not True and not None
 
 
 def test_delete_organization(client: TestClient, session: Session):
@@ -69,10 +136,13 @@ def test_delete_organization(client: TestClient, session: Session):
     jal_vikas = Organization(name="Jal Vikas")
     session.add(jal_vikas)
     session.commit()
-    response = client.delete("/organization/1")
+    response = client.delete(f"/organization/{jal_vikas.id}")
     data = response.json()
     assert response.status_code == 200
-    assert re.search(r"\bdeleted\b", data["message"])
+
+    assert jal_vikas.is_deleted is True
+    assert data["name"] == jal_vikas.name
+    assert data["id"] == jal_vikas.id
     response = client.delete("/organization/2")
     assert response.status_code == 404
     assert response.json() == {"detail": "Organization not found"}
