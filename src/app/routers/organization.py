241924a5be1e7afb63dcd57329cfd105582c12e8
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-08 23:48:06 +0530

    Updated Organization Router

--- src/app/routers/organization.py
+++ src/app/routers/organization.py
@@ -1,17 +1,23 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session, select
 
 from app.database import get_session
-from app.models.organization import Organization
+from app.models.organization import (
+    Organization,
+    OrganizationCreate,
+    OrganizationPublic,
+    OrganizationUpdate,
+)
 
 router = APIRouter(prefix="/organization", tags=["Organization"])
 
 
 # Create a Organization
-@router.post("/", response_model=Organization)
+@router.post("/", response_model=OrganizationPublic)
 def create_organization(
-    organization: Organization, session: Session = Depends(get_session)
+    organization_create: OrganizationCreate, session: Session = Depends(get_session)
 ):
+    organization = Organization.model_validate(organization_create)
     session.add(organization)
     session.commit()
     session.refresh(organization)
@@ -19,14 +25,14 @@ def create_organization(
 
 
 # Get all Organizations
-@router.get("/", response_model=list[Organization])
+@router.get("/", response_model=list[OrganizationPublic])
 def get_organization(session: Session = Depends(get_session)):
     organization = session.exec(select(Organization)).all()
     return organization
 
 
 # Get Organization by ID
-@router.get("/{organization_id}", response_model=Organization)
+@router.get("/{organization_id}", response_model=OrganizationPublic)
 def get_organization_by_id(
     organization_id: int, session: Session = Depends(get_session)
 ):
@@ -37,17 +43,34 @@ def get_organization_by_id(
 
 
 # Update a Organization
-@router.put("/{organization_id}", response_model=Organization)
+@router.put("/{organization_id}", response_model=OrganizationPublic)
 def update_organization(
     organization_id: int,
-    updated_data: Organization,
+    updated_data: OrganizationUpdate,
     session: Session = Depends(get_session),
 ):
     organization = session.get(Organization, organization_id)
     if not organization:
         raise HTTPException(status_code=404, detail="Organization not found")
-    organization.name = updated_data.name
-    organization.description = updated_data.description
+    organization_data = updated_data.model_dump(exclude_unset=True)
+    organization.sqlmodel_update(organization_data)
+    session.add(organization)
+    session.commit()
+    session.refresh(organization)
+    return organization
+
+
+# Set Visibility of Organization
+@router.patch("/{organization_id}", response_model=OrganizationPublic)
+def visibility_organization(
+    organization_id: int,
+    is_active: bool = Query(False, description="Set visibility of organization"),
+    session: Session = Depends(get_session),
+):
+    organization = session.get(Organization, organization_id)
+    if not organization:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    organization.is_active = is_active
     session.add(organization)
     session.commit()
     session.refresh(organization)
@@ -55,7 +78,7 @@ def update_organization(
 
 
 # Delete a Organization
-@router.delete("/{organization_id}")
+@router.delete("/{organization_id}", response_model=OrganizationPublic)
 def delete_organization(organization_id: int, session: Session = Depends(get_session)):
     organization = session.get(Organization, organization_id)
     if not organization:
@@ -64,4 +87,4 @@ def delete_organization(organization_id: int, session: Session = Depends(get_ses
     session.add(organization)
     session.commit()
     session.refresh(organization)
-    return {"message": "Organization deleted successfully"}
+    return organization
