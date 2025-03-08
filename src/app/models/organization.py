3ed46206abcace2665eb3396b3aafa8e8a9d8382
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-08 23:47:11 +0530

    Organization Model

--- src/app/models/organization.py
+++ src/app/models/organization.py
@@ -3,7 +3,12 @@ from datetime import datetime, timezone
 from sqlmodel import Field, SQLModel
 
 
-class Organization(SQLModel, table=True):
+class OrganizationBase(SQLModel):
+    name: str = Field(nullable=False)
+    description: str | None = Field(default=None, nullable=True)
+
+
+class Organization(OrganizationBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     created_date: datetime | None = Field(
         default_factory=lambda: datetime.now(timezone.utc)
@@ -12,7 +17,24 @@ class Organization(SQLModel, table=True):
         default_factory=lambda: datetime.now(timezone.utc),
         sa_column_kwargs={"onupdate": datetime.now(timezone.utc)},
     )
-    is_active: bool | None = Field(nullable=True)
-    name: str = Field(nullable=False)
-    description: str | None = Field(default=None, nullable=True)
+    is_active: bool | None = Field(default=None, nullable=True)
     is_deleted: bool = Field(default=False, nullable=False)
+
+
+class OrganizationCreate(OrganizationBase):
+    pass
+
+
+class OrganizationPublic(OrganizationBase):
+    id: int | None
+    created_date: datetime | None
+    modified_date: datetime | None
+    name: str | None
+    description: str | None
+    is_active: bool | None
+    is_deleted: bool | None
+
+
+class OrganizationUpdate(OrganizationBase):
+    name: str | None
+    description: str | None
