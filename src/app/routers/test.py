ae82e1631d03f29e933fba2305740c2216535d5d
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-09 19:23:44 +0530

    Updated Test POST Router to support Question Read

--- src/app/routers/test.py
+++ src/app/routers/test.py
@@ -2,7 +2,13 @@ from fastapi import APIRouter, Body, Depends
 from sqlmodel import Session, select
 
 from app.database import get_session
-from app.models.test import Test, TestCreate, TestPublic, TestTagLink
+from app.models.test import (
+    Test,
+    TestCreate,
+    TestPublic,
+    TestQuestionStaticLink,
+    TestTagLink,
+)
 
 router = APIRouter(prefix="/test", tags=["Test"])
 
@@ -11,11 +17,13 @@ router = APIRouter(prefix="/test", tags=["Test"])
 @router.post("/", response_model=TestPublic)
 def create_test(
     test_create: TestCreate,
-    tag_ids: list[int] = Body(default=[]),
+    tag_ids: list[int] = Body(default=[], description="IDs off associated Tags"),
+    question_ids: list[int] = Body(
+        default=[], description="IDs off associated Question"
+    ),
     session: Session = Depends(get_session),
 ):
     test = Test.model_validate(test_create)
-    print("test-->", test)
     session.add(test)
     session.commit()
     session.refresh(test)
@@ -28,4 +36,28 @@ def create_test(
         select(TestTagLink.tag_id).where(TestTagLink.test_id == test.id)
     )
 
-    return TestPublic(**test.model_dump(), tags=stored_tag_ids)
+    question_links = [
+        TestQuestionStaticLink(test_id=test.id, question_id=question_id)
+        for question_id in question_ids
+    ]
+    session.add_all(question_links)
+    session.commit()
+
+    stored_question_ids = session.exec(
+        select(TestQuestionStaticLink.question_id).where(
+            TestQuestionStaticLink.test_id == test.id
+        )
+    )
+
+    return TestPublic(
+        **test.model_dump(),
+        tags=stored_tag_ids,
+        test_question_static=stored_question_ids,
+    )
+
+
+# Get All Tests
+@router.get("/", response_model=list[TestPublic])
+def get_test(session: Session = Depends(get_session)):
+    states = session.exec(select(Test)).all()
+    return states
