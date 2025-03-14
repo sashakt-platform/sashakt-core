6e3388660d580ae93d65ad0ef9d56b53a20d1954
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-14 22:52:18 +0530

    Route for location

---
+++ backend/app/api/routes/location.py
@@ -0,0 +1,234 @@
+from typing import Any
+
+from fastapi import APIRouter, HTTPException
+from sqlmodel import select
+
+from app.api.deps import SessionDep
+from app.models.location import (
+    Block,
+    BlockCreate,
+    BlockPublic,
+    BlockUpdate,
+    Country,
+    CountryCreate,
+    CountryPublic,
+    CountryUpdate,
+    District,
+    DistrictCreate,
+    DistrictPublic,
+    DistrictUpdate,
+    State,
+    StateCreate,
+    StatePublic,
+    StateUpdate,
+)
+
+router = APIRouter(prefix="/location", tags=["Location"])
+
+country_router = APIRouter()
+state_router = APIRouter()
+district_router = APIRouter()
+block_router = APIRouter()
+
+
+# Create a Country
+@country_router.post("/", response_model=CountryPublic)
+def create_country(country: CountryCreate, session: SessionDep):
+    db_country = Country.model_validate(country)
+    session.add(db_country)
+    session.commit()
+    session.refresh(db_country)
+    return db_country
+
+
+# Get all Countries
+@country_router.get("/", response_model=list[CountryPublic])
+def get_countries(session: SessionDep):
+    countries = session.exec(select(Country)).all()
+    return countries
+
+
+# Get Country by ID
+@country_router.get("/{country_id}", response_model=CountryPublic)
+def get_country_by_id(country_id: int, session: SessionDep):
+    country = session.get(Country, country_id)
+    if not country:
+        raise HTTPException(status_code=404, detail="Country not found")
+    return country
+
+
+# Update a Country
+@country_router.put("/{country_id}", response_model=CountryPublic)
+def update_country(country_id: int, country: CountryUpdate, session: SessionDep):
+    country_db = session.get(Country, country_id)
+    if not country_db:
+        raise HTTPException(status_code=404, detail="Country not found")
+    country_data = country.model_dump(exclude_unset=True)
+    country_db.sqlmodel_update(country_data)
+    session.add(country_db)
+    session.commit()
+    session.refresh(country_db)
+    return country_db
+
+
+# ------- State Level Routes ------
+
+
+# Create a State
+@state_router.post("/", response_model=StatePublic)
+def create_state(
+    *,
+    state_create: StateCreate,
+    session: SessionDep,
+) -> Any:
+    state = State.model_validate(state_create)
+    session.add(state)
+    session.commit()
+    session.refresh(state)
+    return state
+
+
+# Get all States
+@state_router.get("/", response_model=list[StatePublic])
+def get_state(session: SessionDep):
+    states = session.exec(select(State)).all()
+    return states
+
+
+# Get State by ID
+@state_router.get("/{state_id}", response_model=StatePublic)
+def get_state_by_id(state_id: int, session: SessionDep):
+    state = session.get(State, state_id)
+    if not state:
+        raise HTTPException(status_code=404, detail="State not found")
+    return state
+
+
+# Update State by ID
+@state_router.put("/{state_id}", response_model=StatePublic)
+def update_state(
+    *,
+    state_id: int,
+    state_update: StateUpdate,
+    session: SessionDep,
+) -> Any:
+    state_db = session.get(State, state_id)
+    if not state_db:
+        raise HTTPException(status_code=404, detail="State not found")
+    state_data = state_update.model_dump(exclude_unset=True)
+    state_db.sqlmodel_update(state_data)
+    session.add(state_db)
+    session.commit()
+    session.refresh(state_db)
+    return state_db
+
+
+#  -------- District Level Routes ------
+
+
+# Create a District
+@district_router.post("/", response_model=DistrictPublic)
+def create_district(
+    *,
+    district_create: DistrictCreate,
+    session: SessionDep,
+) -> Any:
+    district = District.model_validate(district_create)
+    session.add(district)
+    session.commit()
+    session.refresh(district)
+    return district
+
+
+# Get all Districts
+@district_router.get("/", response_model=list[DistrictPublic])
+def get_district(session: SessionDep):
+    districts = session.exec(select(District)).all()
+    return districts
+
+
+# Get District by ID
+@district_router.get("/{district_id}", response_model=DistrictPublic)
+def get_district_by_id(district_id: int, session: SessionDep):
+    district = session.get(District, district_id)
+    if not district:
+        raise HTTPException(status_code=404, detail="District not found")
+    return district
+
+
+# Update District by ID
+@district_router.put("/{district_id}", response_model=DistrictPublic)
+def update_district(
+    *,
+    district_id: int,
+    district_update: DistrictUpdate,
+    session: SessionDep,
+) -> Any:
+    district_db = session.get(District, district_id)
+    if not district_db:
+        raise HTTPException(status_code=404, detail="District not found")
+    district_data = district_update.model_dump(exclude_unset=True)
+    district_db.sqlmodel_update(district_data)
+    session.add(district_db)
+    session.commit()
+    session.refresh(district_db)
+    return district_db
+
+
+#  -------- Block Level Routes ------
+
+
+# Create a Block
+@block_router.post("/", response_model=BlockPublic)
+def create_block(
+    *,
+    block_create: BlockCreate,
+    session: SessionDep,
+) -> Any:
+    block = Block.model_validate(block_create)
+    session.add(block)
+    session.commit()
+    session.refresh(block)
+    return block
+
+
+# Get all Blocks
+@block_router.get("/", response_model=list[BlockPublic])
+def get_block(session: SessionDep):
+    blocks = session.exec(select(Block)).all()
+    return blocks
+
+
+# Get Block by ID
+@block_router.get("/{block_id}", response_model=BlockPublic)
+def get_block_by_id(block_id: int, session: SessionDep):
+    block = session.get(Block, block_id)
+    if not block:
+        raise HTTPException(status_code=404, detail="Block not found")
+    return block
+
+
+# Update Block by ID
+@block_router.put("/{block_id}", response_model=BlockPublic)
+def update_block(
+    *,
+    block_id: int,
+    block_update: BlockUpdate,
+    session: SessionDep,
+) -> Any:
+    block_db = session.get(Block, block_id)
+    if not block_db:
+        raise HTTPException(status_code=404, detail="Block not found")
+    block_data = block_update.model_dump(exclude_unset=True)
+    block_db.sqlmodel_update(block_data)
+    session.add(block_db)
+    session.commit()
+    session.refresh(block_db)
+    return block_db
+
+
+# Include all routers
+router.include_router(country_router, prefix="/country", tags=["Country"])
+router.include_router(state_router, prefix="/state", tags=["State"])
+router.include_router(district_router, prefix="/district", tags=["District"])
+router.include_router(block_router, prefix="/block", tags=["Block"])
