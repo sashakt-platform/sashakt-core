5850fc5ebd7a745b4de39054e4e866c8e9754de2
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-14 22:49:25 +0530

    Updated main.py to include location router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, organization, private, roles, users, utils
+from app.api.routes import location, login, organization, private, roles, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -9,6 +9,7 @@ api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(roles.router)
 api_router.include_router(organization.router)
+api_router.include_router(location.router)
 
 
 if settings.ENVIRONMENT == "local":
