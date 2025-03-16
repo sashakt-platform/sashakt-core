db47ad7fc84db4a165ad54c27a3b973c7847d81b
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-16 10:36:28 +0530

    Creating dummy models for tag and question

---
+++ backend/app/models/tag.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field, Relationship, SQLModel
+
+from app.models.test import TestTagLink
+
+if TYPE_CHECKING:
+    from app.models.test import Test
+
+
+class Tag(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(nullable=False)
+    tests: list["Test"] = Relationship(back_populates="tags", link_model=TestTagLink)
