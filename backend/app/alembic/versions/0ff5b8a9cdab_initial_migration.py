06604cf8a4c28b0a0d34fbef748eaa4f8422ba92
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-03-18 23:29:20 +0000

    add missing schema tables

--- backend/app/alembic/versions/0ff5b8a9cdab_initial_migration.py
+++ backend/app/alembic/versions/0ff5b8a9cdab_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: 1e86265e426c
+Revision ID: 0ff5b8a9cdab
 Revises:
-Create Date: 2025-03-16 23:26:42.170060
+Create Date: 2025-03-18 23:28:16.832734
 
 """
 from alembic import op
@@ -11,7 +11,7 @@ import sqlmodel.sql.sqltypes
 
 
 # revision identifiers, used by Alembic.
-revision = '1e86265e426c'
+revision = '0ff5b8a9cdab'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -27,6 +27,7 @@ def upgrade():
     sa.Column('is_active', sa.Boolean(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_country_name'), 'country', ['name'], unique=False)
     op.create_table('organization',
     sa.Column('name', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
     sa.Column('description', sqlmodel.sql.sqltypes.AutoString(), nullable=True),
@@ -37,11 +38,13 @@ def upgrade():
     sa.Column('is_deleted', sa.Boolean(), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_organization_name'), 'organization', ['name'], unique=False)
     op.create_table('question',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('question', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_question_question'), 'question', ['question'], unique=False)
     op.create_table('role',
     sa.Column('description', sqlmodel.sql.sqltypes.AutoString(length=255), nullable=True),
     sa.Column('id', sa.Integer(), nullable=False),
@@ -53,6 +56,7 @@ def upgrade():
     sa.Column('name', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_tag_name'), 'tag', ['name'], unique=False)
     op.create_table('user',
     sa.Column('full_name', sqlmodel.sql.sqltypes.AutoString(length=255), nullable=True),
     sa.Column('email', sqlmodel.sql.sqltypes.AutoString(length=255), nullable=False),
@@ -62,6 +66,16 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_user_email'), 'user', ['email'], unique=True)
+    op.create_table('candidate',
+    sa.Column('user_id', sa.Integer(), nullable=True),
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('created_date', sa.DateTime(), nullable=True),
+    sa.Column('modified_date', sa.DateTime(), nullable=True),
+    sa.Column('is_active', sa.Boolean(), nullable=True),
+    sa.Column('is_deleted', sa.Boolean(), nullable=False),
+    sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE'),
+    sa.PrimaryKeyConstraint('id')
+    )
     op.create_table('state',
     sa.Column('name', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
     sa.Column('country_id', sa.Integer(), nullable=False),
@@ -72,6 +86,7 @@ def upgrade():
     sa.ForeignKeyConstraint(['country_id'], ['country.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_state_name'), 'state', ['name'], unique=False)
     op.create_table('test',
     sa.Column('name', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
     sa.Column('description', sqlmodel.sql.sqltypes.AutoString(), nullable=True),
@@ -100,6 +115,7 @@ def upgrade():
     sa.ForeignKeyConstraint(['template_id'], ['test.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_test_name'), 'test', ['name'], unique=False)
     op.create_table('district',
     sa.Column('name', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
     sa.Column('state_id', sa.Integer(), nullable=False),
@@ -110,23 +126,24 @@ def upgrade():
     sa.ForeignKeyConstraint(['state_id'], ['state.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_district_name'), 'district', ['name'], unique=False)
     op.create_table('test_question',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('created_date', sa.DateTime(), nullable=True),
-    sa.Column('test_id', sa.Integer(), nullable=True),
-    sa.Column('question_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['question_id'], ['question.id'], ),
-    sa.ForeignKeyConstraint(['test_id'], ['test.id'], ),
+    sa.Column('test_id', sa.Integer(), nullable=False),
+    sa.Column('question_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['question_id'], ['question.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['test_id'], ['test.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('test_id', 'question_id')
     )
     op.create_table('test_state',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('created_date', sa.DateTime(), nullable=True),
-    sa.Column('test_id', sa.Integer(), nullable=True),
-    sa.Column('state_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['state_id'], ['state.id'], ),
-    sa.ForeignKeyConstraint(['test_id'], ['test.id'], ),
+    sa.Column('test_id', sa.Integer(), nullable=False),
+    sa.Column('state_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['state_id'], ['state.id'], ondelete='CASCADE'),
+    sa.ForeignKeyConstraint(['test_id'], ['test.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('test_id', 'state_id')
     )
@@ -150,23 +167,33 @@ def upgrade():
     sa.ForeignKeyConstraint(['district_id'], ['district.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_index(op.f('ix_block_name'), 'block', ['name'], unique=False)
     # ### end Alembic commands ###
 
 
 def downgrade():
     # ### commands auto generated by Alembic - please adjust! ###
+    op.drop_index(op.f('ix_block_name'), table_name='block')
     op.drop_table('block')
     op.drop_table('test_tag')
     op.drop_table('test_state')
     op.drop_table('test_question')
+    op.drop_index(op.f('ix_district_name'), table_name='district')
     op.drop_table('district')
+    op.drop_index(op.f('ix_test_name'), table_name='test')
     op.drop_table('test')
+    op.drop_index(op.f('ix_state_name'), table_name='state')
     op.drop_table('state')
+    op.drop_table('candidate')
     op.drop_index(op.f('ix_user_email'), table_name='user')
     op.drop_table('user')
+    op.drop_index(op.f('ix_tag_name'), table_name='tag')
     op.drop_table('tag')
     op.drop_table('role')
+    op.drop_index(op.f('ix_question_question'), table_name='question')
     op.drop_table('question')
+    op.drop_index(op.f('ix_organization_name'), table_name='organization')
     op.drop_table('organization')
+    op.drop_index(op.f('ix_country_name'), table_name='country')
     op.drop_table('country')
     # ### end Alembic commands ###
