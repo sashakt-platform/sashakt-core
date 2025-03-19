60f59a292fa9afa99f5322accc8a46b4774c822e
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-19 17:51:28 +0530

    Changes in Dummy Table of Question to support CandidateTestQuestion Model

--- backend/app/models/question.py
+++ backend/app/models/question.py
@@ -2,11 +2,12 @@ from typing import TYPE_CHECKING
 
 from sqlmodel import Field, Relationship, SQLModel
 
+from app.models import CandidateTest, CandidateTestQuestion
 from app.models.test import TestQuestion
 
 if TYPE_CHECKING:
-    from app.models.test import (
-        Test,  # Import only at runtime
+    from app.models import (
+        Test,
     )
 
 
@@ -15,4 +16,7 @@ class Question(SQLModel, table=True):
     question: str = Field(nullable=False, index=True)
     tests: list["Test"] = Relationship(
         back_populates="test_question_static", link_model=TestQuestion
+    )
+    candidate_test: list["CandidateTest"] = Relationship(
+        back_populates="quesion_revision", link_model=CandidateTestQuestion
     )
