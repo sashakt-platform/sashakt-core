ac5856b186a445f100a82adcead5346dbefdd239
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-25 17:07:12 +0530

    writing more assertions for test caes for role -

--- backend/app/tests/api/routes/test_role.py
+++ backend/app/tests/api/routes/test_role.py
@@ -1,4 +1,4 @@
-import uuid
+import re
 
 from fastapi.testclient import TestClient
 from sqlmodel import Session
@@ -11,6 +11,7 @@ def test_create_role(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
     data = {"name": "Foo", "description": "Fighters"}
+
     response = client.post(
         f"{settings.API_V1_STR}/roles/",
         headers=superuser_token_headers,
@@ -35,25 +36,35 @@ def test_read_role(
     content = response.json()
     assert content["name"] == role.name
     assert content["description"] == role.description
-    assert content["id"] == str(role.id)
+    assert content["id"] == role.id
+    assert "created_date" in content
+    assert "modified_date" in content
+    assert content["is_active"] is None
+    assert content["is_deleted"] is False
 
 
 def test_read_role_not_found(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
     response = client.get(
-        f"{settings.API_V1_STR}/roles/111111111",
+        f"{settings.API_V1_STR}/roles/-1",
         headers=superuser_token_headers,
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == "role not found"
+    assert re.search(r"\bnot\b.*\bfound\b", content["detail"])
 
 
 def test_read_role_not_enough_permissions(
-    client: TestClient, normal_user_token_headers: dict[str, str], db: Session
+    client: TestClient,
+    normal_user_token_headers: dict[str, str],
+    db: Session,
+    superuser_token_headers: dict[str, str],
 ) -> None:
     role = create_random_role(db)
+    print("Random role is ", role)
+    print("normal_user_token_headers->", normal_user_token_headers)
+    print("superuser_token_headers->", superuser_token_headers)
     response = client.get(
         f"{settings.API_V1_STR}/roles/{role.id}",
         headers=normal_user_token_headers,
@@ -91,7 +102,7 @@ def test_update_role(
     content = response.json()
     assert content["name"] == data["name"]
     assert content["description"] == data["description"]
-    assert content["id"] == str(role.id)
+    assert content["id"] == role.id
 
 
 def test_update_role_not_found(
@@ -99,13 +110,13 @@ def test_update_role_not_found(
 ) -> None:
     data = {"name": "Updated name", "description": "Updated description"}
     response = client.put(
-        f"{settings.API_V1_STR}/roles/{uuid.uuid4()}",
+        f"{settings.API_V1_STR}/roles/-1",
         headers=superuser_token_headers,
         json=data,
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == "role not found"
+    assert re.search(r"\bnot\b.*\bfound\b", content["detail"])
 
 
 def test_update_role_not_enough_permissions(
@@ -133,19 +144,27 @@ def test_delete_role(
     )
     assert response.status_code == 200
     content = response.json()
-    assert content["message"] == "role deleted successfully"
+    assert re.search(r"delete", content["message"])
+
+    response = client.delete(
+        f"{settings.API_V1_STR}/roles/{role.id}",
+        headers=superuser_token_headers,
+    )
+    assert response.status_code == 404
+    content = response.json()
+    assert re.search(r"\bnot\b.*\bfound\b", content["detail"])
 
 
 def test_delete_role_not_found(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
     response = client.delete(
-        f"{settings.API_V1_STR}/roles/{uuid.uuid4()}",
+        f"{settings.API_V1_STR}/roles/-1",
         headers=superuser_token_headers,
     )
     assert response.status_code == 404
     content = response.json()
-    assert content["detail"] == "role not found"
+    assert re.search(r"\bnot\b.*\bfound\b", content["detail"])
 
 
 def test_delete_role_not_enough_permissions(
