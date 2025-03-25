a10c57d3400d87a58bab7fb5e1757e4baae1cb24
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-03-25 23:19:30 +0000

    code clean up

--- backend/app/tests/utils/role.py
+++ backend/app/tests/utils/role.py
@@ -1,12 +1,17 @@
 from sqlmodel import Session
 
-from app import crud
 from app.models import Role, RoleCreate
 from app.tests.utils.utils import random_lower_string
 
 
-def create_random_role(db: Session) -> Role:
+def create_random_role(session: Session) -> Role:
     name = random_lower_string()
     description = random_lower_string()
     role_in = RoleCreate(name=name, description=description)
-    return crud.create_role(session=db, role_in=role_in)
+
+    db_role = Role.model_validate(role_in)
+    session.add(db_role)
+    session.commit()
+    session.refresh(db_role)
+
+    return db_role
