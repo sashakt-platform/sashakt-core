5d5da06c9252a6457c9838bab795b531493bdb3d
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-25 16:41:44 +0530

    Updates to Role Model

--- backend/app/models/role.py
+++ backend/app/models/role.py
@@ -1,10 +1,26 @@
-from sqlmodel import Field, SQLModel
+from datetime import datetime, timezone
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field, Relationship, SQLModel
+
+if TYPE_CHECKING:
+    from app.models import User
 
 
 # Shared properties
 class RoleBase(SQLModel):
-    name: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default=None, max_length=255)
+    name: str = Field(
+        min_length=1,
+        max_length=255,
+        title="Name of Role",
+        description="Name of  the Role",
+    )
+    description: str | None = Field(
+        default=None,
+        max_length=255,
+        title="Description of Role",
+        description="A brief Description of the Role",
+    )
 
 
 # Properties to receive on name creation
@@ -14,18 +30,31 @@ class RoleCreate(RoleBase):
 
 # Properties to receive on name update
 class RoleUpdate(RoleBase):
-    title: str | None = Field(default=None, min_length=1, max_length=255)
+    pass
 
 
 # Database model, database table inferred from class name
 class Role(RoleBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(max_length=255)
+    created_date: datetime | None = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
+    modified_date: datetime | None = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column_kwargs={"onupdate": datetime.now(timezone.utc)},
+    )
+    is_active: bool | None = Field(default=None, nullable=True)
+    is_deleted: bool = Field(default=False, nullable=False)
+    users: list["User"] = Relationship(back_populates="role")
 
 
 # Properties to return via API, id is always required
 class RolePublic(RoleBase):
-    id: int | None = Field(default=None, primary_key=True)
+    id: int
+    created_date: datetime
+    modified_date: datetime
+    is_active: bool | None
+    is_deleted: bool
 
 
 class RolesPublic(SQLModel):
