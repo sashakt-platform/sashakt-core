8184a2b10db61390444172016d6077f902f5d562
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-28 18:40:04 +0530

    Creating initial data including role creation

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -2,7 +2,7 @@ from sqlmodel import Session, create_engine, select
 
 from app import crud
 from app.core.config import settings
-from app.models import User, UserCreate
+from app.models import Role, User, UserCreate
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
@@ -13,12 +13,32 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 
 def init_db(session: Session) -> None:
+    # Checking if super-admin roles are created
+    super_admin = session.exec(select(Role).where(Role.name == "super_admin")).first()
+    if not super_admin:
+        # Handle the case where no role with label 'super-admin' exists
+        super_admin = Role(
+            label="Super Admin",
+            name="super_admin",
+            description="A super-admin has overall access to the system",
+        )
+        session.add(super_admin)
+        session.commit()
+        session.refresh(super_admin)
+        super_admin_role_id = super_admin.id
+    else:
+        super_admin_role_id = super_admin.id
+
     user = session.exec(
         select(User).where(User.email == settings.FIRST_SUPERUSER)
     ).first()
     if not user:
         user_in = UserCreate(
+            full_name=settings.FIRST_SUPERUSER_FULLNAME,
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
+            role_id=super_admin_role_id,
+            phone=settings.FIRST_SUPERUSER_MOBILE,
+            created_by_id=None,
         )
         user = crud.create_user(session=session, user_create=user_in)
