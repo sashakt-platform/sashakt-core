9c487dc126d7c4bce08bda140f096df56586f6d4
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-03-29 18:45:56 +0530

    Test Case for Role visibility

--- backend/app/tests/api/routes/test_role.py
+++ backend/app/tests/api/routes/test_role.py
@@ -3,12 +3,17 @@ from sqlmodel import Session
 
 from app.core.config import settings
 from app.tests.utils.role import create_random_role
+from app.tests.utils.utils import random_lower_string
 
 
 def test_create_role(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
-    data = {"name": "Foo", "description": "Fighters"}
+    data = {
+        "name": random_lower_string(),
+        "description": random_lower_string(),
+        "label": random_lower_string(),
+    }
     response = client.post(
         f"{settings.API_V1_STR}/roles/",
         headers=superuser_token_headers,
@@ -33,6 +38,7 @@ def test_read_role(
     content = response.json()
     assert content["name"] == role.name
     assert content["description"] == role.description
+    assert content["label"] == role.label
     assert content["id"] == role.id
 
 
@@ -81,7 +87,11 @@ def test_update_role(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
     role = create_random_role(db)
-    data = {"name": "Updated name", "description": "Updated description"}
+    data = {
+        "name": random_lower_string(),
+        "description": random_lower_string(),
+        "label": random_lower_string(),
+    }
     response = client.put(
         f"{settings.API_V1_STR}/roles/{role.id}",
         headers=superuser_token_headers,
@@ -91,13 +101,18 @@ def test_update_role(
     content = response.json()
     assert content["name"] == data["name"]
     assert content["description"] == data["description"]
+    assert content["label"] == data["label"]
     assert content["id"] == role.id
 
 
 def test_update_role_not_found(
     client: TestClient, superuser_token_headers: dict[str, str]
 ) -> None:
-    data = {"name": "Updated name", "description": "Updated description"}
+    data = {
+        "name": random_lower_string(),
+        "description": random_lower_string(),
+        "label": random_lower_string(),
+    }
     response = client.put(
         f"{settings.API_V1_STR}/roles/0",
         headers=superuser_token_headers,
@@ -106,6 +121,32 @@ def test_update_role_not_found(
     assert response.status_code == 404
     content = response.json()
     assert content["detail"] == "Role not found"
+
+
+def test_visibility_role(
+    client: TestClient, superuser_token_headers: dict[str, str], db: Session
+) -> None:
+    role = create_random_role(db)
+    data = {"is_active": False}
+    response = client.patch(
+        f"{settings.API_V1_STR}/roles/{role.id}",
+        headers=superuser_token_headers,
+        params=data,
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["is_active"] is False
+    assert content["name"] == role.name
+    assert content["description"] == role.description
+    assert content["label"] == role.label
+    response = client.patch(
+        f"{settings.API_V1_STR}/roles/{role.id}",
+        headers=superuser_token_headers,
+        params={"is_active": True},
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["is_active"] is True
 
 
 # TODO: Fix this once we have permisions in place
