3d5204c5fb33c0bb5bad07e698e650761054f1e7
Ninad Khanolkar <47816457+ninad-ict@users.noreply.github.com>
2025-05-05 08:13:35 +0530

    Helper function to create query with ordering and pagination

--- backend/app/api/routes/utils.py
+++ backend/app/api/routes/utils.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic.networks import EmailStr
 
 from app.api.deps import get_current_active_superuser
@@ -29,3 +29,19 @@ def test_email(email_to: EmailStr) -> Message:
 @router.get("/health-check/")
 async def health_check() -> bool:
     return True
+
+
+# Helper Function to refine query by applying ordering and pagination
+def get_refined_query(query, Table, filters):
+    for order in filters.order_by:
+        is_desc = order.startswith("-")
+        order = order.lstrip("-")
+        column = getattr(Table, order, None)
+        if column is None:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Invalid order_by field: {order}",
+            )
+        query = query.order_by(column.desc() if is_desc else column)
+    query = query.offset(filters.skip).limit(filters.limit)
+    return query
