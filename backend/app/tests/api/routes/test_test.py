5d69f1c79140b521d47114df4e15f519caaee1f8
suryabulusu <teja.surya59@gmail.com>
2026-04-02 13:21:11 +0530

    Revert "Add flat test regression coverage"
    This reverts commit 86f9ef66895753604ee01e4ddaee2b589fc99df8.

--- backend/app/tests/api/routes/test_test.py
+++ backend/app/tests/api/routes/test_test.py
@@ -234,7 +234,6 @@ def test_create_test(
     assert "created_date" in data
     assert "modified_date" in data
     assert "tags" in data
-    assert data["question_sets"] is None
     assert len(data["tags"]) == 2
     assert [data["tags"][0]["name"], data["tags"][1]["name"]] == [
         tag_hindi.name,
@@ -326,7 +325,6 @@ def test_create_test(
     assert "modified_date" in data
     assert "tags" in data
     assert "states" in data
-    assert data["question_sets"] is None
     assert len(data["tags"]) == 2
     assert len(data["states"]) == 2
     assert len(data["question_revisions"]) == 2
