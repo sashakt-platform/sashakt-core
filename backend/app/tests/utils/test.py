ed1a6388be064e23274c75489115ca6b990c546e
Ninad Khanolkar <ninad1701@gmail.com>
2026-04-21 16:41:38 +0530

    Helper function for getting UUID for a test

---
+++ backend/app/tests/utils/test.py
@@ -0,0 +1,20 @@
+import uuid
+
+from sqlmodel import Session
+
+from app.models.test import TestLink
+
+
+def get_test_link(
+    session: Session, test_id: int | None, admin_id: int | None
+) -> TestLink:
+    assert test_id, admin_id is not None
+    test_link = TestLink(
+        test_id=test_id,
+        admin_id=admin_id,
+        uuid=str(uuid.uuid4()),
+    )
+    session.add(test_link)
+    session.commit()
+    session.refresh(test_link)
+    return test_link
