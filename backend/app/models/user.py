d44db15346c07e4e9e07c51892f1caa17df48fa1
Ninad Khanolkar <ninad1701@gmail.com>
2026-04-22 11:04:16 +0530

    Cascade rule for test link

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -124,7 +124,9 @@ class User(UserBase, table=True):
     created_by_id: int | None = Field(default=None, foreign_key="user.id")
     organization_id: int = Field(foreign_key="organization.id")
     tests: list["Test"] | None = Relationship(back_populates="created_by")
-    test_links: list["TestLink"] | None = Relationship(back_populates="admin")
+    test_links: list["TestLink"] | None = Relationship(
+        back_populates="admin", cascade_delete=True
+    )
     candidates: list["Candidate"] = Relationship(back_populates="user")
     tag_types: list["TagType"] = Relationship(back_populates="created_by")
     tags: list["Tag"] = Relationship(back_populates="created_by")
