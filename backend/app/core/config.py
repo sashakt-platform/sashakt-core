0bd63ad2edd64813db2731b3afde5e05ee9cbd71
Ninad Khanolkar <ninad1701@gmail.com>
2026-06-30 16:03:20 +0530

    Make DB engine parameters configurable

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -58,6 +58,9 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
+    DB_POOL_SIZE: int = 20
+    DB_MAX_OVERFLOW: int = 40
+    DB_POOL_TIMEOUT: int = 30
 
     @computed_field  # type: ignore[prop-decorator]
     @property
